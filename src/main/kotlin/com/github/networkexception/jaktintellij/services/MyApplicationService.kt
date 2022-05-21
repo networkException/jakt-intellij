a1d20542613f4173e9660892cb42195b51471cb7
GitHub Action <action@github.com>
2022-05-21 09:29:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/networkexception/jaktintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.networkexception.jaktintellij.services
+
+import com.github.networkexception.jaktintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/networkexception/jaktintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/networkexception/jaktintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.networkexception.jaktintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.networkexception.jaktintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
